309322fd5c0d559bfde4a99887cf3f02ea561435
winlin <winlin@vip.126.com>
2013-12-22 11:58:30 +0800

    update the bootstrap wizard, use v2

--- trunk/research/players/js/srs.js
+++ trunk/research/players/js/srs.js
@@ -20,6 +20,7 @@ function padding(number, length, prefix) {
 function update_nav() {
     $("#nav_srs_player").attr("href", "srs_player.html" + window.location.search);
     $("#nav_srs_publisher").attr("href", "srs_publisher.html" + window.location.search);
+    $("#nav_srs_chat").attr("href", "srs_chat.html" + window.location.search);
     $("#nav_srs_bwt").attr("href", "srs_bwt.html" + window.location.search);
     $("#nav_jwplayer6").attr("href", "jwplayer6.html" + window.location.search);
     $("#nav_osmf").attr("href", "osmf.html" + window.location.search);
