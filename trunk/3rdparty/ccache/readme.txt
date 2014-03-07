42ad2641829aebccbe4aa73e20c8a142db984ebd
winlin <winlin@vip.126.com>
2014-03-07 16:09:35 +0800

    add gprof files and dot files

---
+++ trunk/3rdparty/ccache/readme.txt
@@ -0,0 +1,11 @@
+ccache是samba组织提供的加速编译过程的工具，
+使用虚拟机编译可以考虑用这个工具，让编译过程飞快。
+
+链接：
+    http://ccache.samba.org/
+    http://samba.org/ftp/ccache/ccache-3.1.9.tar.xz
+    http://ccache.samba.org/manual.html
+
+安装方法：
+    bash build_ccache.sh
+注意：要求以sudoer执行，要修改文件。
---
+++ trunk/3rdparty/gprof/build_gprof2dot.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# check exists.
+if [[ -d graphviz-2.18 ]]; then 
+    echo "graphviz is ok";
+    exit 0;
+fi
+
+# check sudoer.
+sudo echo "ok" > /dev/null 2>&1; 
+ret=$?; if [[ 0 -ne ${ret} ]]; then echo "you must be sudoer"; exit 1; fi
+
+tar xf graphviz-2.18.tar.gz
+cd graphviz-2.18 && ./configure && make && sudo make install
+ret=$?; if [[ $ret -ne 0 ]]; then echo "build gprof2dot failed."; exit $ret; fi
+
+echo "we test in Centos6.0, it's ok"
