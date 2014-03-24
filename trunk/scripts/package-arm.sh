bcb54e6587715c37d4e15843e07e2e39cb60f129
winlin <winlin@vip.126.com>
2014-03-24 13:15:22 +0800

    fix bug of package arm

--- trunk/scripts/package-arm.sh
+++ trunk/scripts/package-arm.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-bash package.sh arm
 
+echo "argv[0]=$0"
+if [[ ! -f $0 ]]; then 
+    echo "directly execute the scripts on shell.";
+    work_dir=`pwd`
+else 
+    echo "execute scripts in file: $0";
+    work_dir=`dirname $0`; work_dir=`(cd ${work_dir} && pwd)`
+fi
+
+bash ${work_dir}/package.sh arm
