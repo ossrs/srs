ea3561de08bcce7b0149cb1168ae9bcbdbf6a346
winlin <winlin@vip.126.com>
2014-03-26 17:18:28 +0800

    change script log dir to objs

--- trunk/scripts/_step.start.ffmpeg.players.sh
+++ trunk/scripts/_step.start.ffmpeg.players.sh
@@ -2,10 +2,10 @@
 src_dir='src'
 if [[ ! -d $src_dir ]]; then echo "错误：必须在src同目录执行脚本"; exit 1; fi
 
-cmd="nohup bash ./scripts/_ffmpeg.players.sh >./objs/logs/ffmpeg-players.log 2>&1 &"
+cmd="nohup bash ./scripts/_ffmpeg.players.sh >./objs/ffmpeg-players.log 2>&1 &"
 echo "启动FFMPEG推送players流(播放器上演示用)：$cmd"
 pids=`ps aux|grep scripts|grep "/_ffmpeg.players.sh"|awk '{print $2}'`; for pid in $pids; do echo "结束现有进程：$pid"; kill -s SIGKILL $pid; done
-nohup bash ./scripts/_ffmpeg.players.sh >./objs/logs/ffmpeg-players.log 2>&1 &
+nohup bash ./scripts/_ffmpeg.players.sh >./objs/ffmpeg-players.log 2>&1 &
 ret=$?; if [[ 0 -ne $ret ]]; then echo "错误：启动FFMPEG推送players流(播放器上演示用)失败"; exit $ret; fi
 
 echo "启动FFMPEG推送players流(播放器上演示用)成功"
