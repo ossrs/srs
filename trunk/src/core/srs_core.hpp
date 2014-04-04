1c02f4551c6e8f15ea9de0487b7c90bad62a9464
winlin <winlin@vip.126.com>
2014-04-04 23:28:31 +0800

    always use 64bits for lseek

--- trunk/src/core/srs_core.hpp
+++ trunk/src/core/srs_core.hpp
@@ -52,6 +52,12 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 * build failed.
 */
 
+// for 32bit os, 2G big file limit for unistd io, 
+// ie. read/write/lseek to use 64bits size for huge file.
+#ifndef _FILE_OFFSET_BITS
+    #define _FILE_OFFSET_BITS 64
+#endif
+
 // for int64_t print using PRId64 format.
 #ifndef __STDC_FORMAT_MACROS
     #define __STDC_FORMAT_MACROS
