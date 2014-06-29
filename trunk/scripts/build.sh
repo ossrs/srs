18534ecdb29777bc085862ed0f069ee3186e2432
winlin <winlin@vip.126.com>
2014-06-29 10:06:28 +0800

    donot start nginx for demo, use cherrypy to delivery HLS.

--- trunk/scripts/build.sh
+++ trunk/scripts/build.sh
@@ -3,8 +3,9 @@ src_dir='src'
 if [[ ! -d $src_dir ]]; then echo "错误：必须在src同目录执行脚本"; exit 1; fi
 
 # step 1: build srs 
+# nginx is not needed, we use cherrypy to delivery hls.
 echo "编译SRS"
-./configure --x86-x64 --with-nginx && make
+./configure --x86-x64 && make
 ret=$?; if [[ 0 -ne $ret ]]; then echo "错误：编译SRS失败"; exit $ret; fi
 
 echo "编译SRS成功"
--- trunk/scripts/stop.sh
+++ trunk/scripts/stop.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-sudo ./objs/nginx/sbin/nginx -s stop
-echo "停止nginx"
-
 ./etc/init.d/srs-demo stop; ret=$?; if [[ 0 -ne $ret ]]; then echo "错误：停止SRS失败"; exit $ret; fi
 echo "停止SRS服务器成功"
 
