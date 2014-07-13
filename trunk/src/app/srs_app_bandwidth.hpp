0322dbbfb29c20104a7d48aa10fe8b013980d252
winlin <winlin@vip.126.com>
2014-07-13 12:09:26 +0800

    big refine code, add author to bandwidth test/check.

--- trunk/src/app/srs_app_bandwidth.hpp
+++ trunk/src/app/srs_app_bandwidth.hpp
@@ -2,6 +2,7 @@
 The MIT License (MIT)
 
 Copyright (c) 2013-2014 wenjiegit
+Copyright (c) 2013-2014 winlin
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of
 this software and associated documentation files (the "Software"), to deal in
