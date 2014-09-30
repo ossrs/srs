967de9d2e7f4d8dfb3ef6adc0244b81b63d92fe2
winlin <winlin@vip.126.com>
2014-09-30 16:34:46 +0800

    for bug #159: postpone the http server bug to 2.0+

--- trunk/src/main/srs_main_server.cpp
+++ trunk/src/main/srs_main_server.cpp
@@ -190,6 +190,12 @@ int main(int argc, char** argv)
     
     // features
     show_macro_features();
+
+    // for special features.
+#ifdef SRS_AUTO_HTTP_SERVER
+    srs_warn("http server is dev feature, "
+        "@see https://github.com/winlinvip/simple-rtmp-server/wiki/HTTPServer#feature");
+#endif
     
     /**
     * we do nothing in the constructor of server,
