b8db8b0fa41df44488b2b82c8432b148245b664c
winlin <winlin@vip.126.com>
2014-10-27 11:32:34 +0800

    add oschina mirror

--- trunk/scripts/oschina.mirror.sh
+++ trunk/scripts/oschina.mirror.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "更新CSDN镜像的脚本"
+echo "更新OSChina镜像的脚本"
 
 echo "argv[0]=$0"
 if [[ ! -f $0 ]]; then 
@@ -20,22 +20,22 @@ cd $work_dir
 ret=$?; if [[ $ret -ne 0 ]]; then exit $ret; fi
 ok_msg "导入脚本成功"
 
-git remote -v|grep code.csdn.net >/dev/null 2>&1
+git remote -v|grep git.oschina.net >/dev/null 2>&1
 ret=$?; if [[ 0 -ne $ret ]]; then 
-    failed_msg "当前分支不是CSDN镜像"; 
+    failed_msg "当前分支不是OSChina镜像"; 
     cat <<END
-创建CSDN镜像的过程如下：
-1. 在CSDN上创建项目，从https://github.com/winlinvip/simple-rtmp-server拷贝过来。
+创建OSChina镜像的过程如下：
+1. 在OSChina上创建项目，从https://github.com/winlinvip/simple-rtmp-server拷贝过来。
 2. 在本地虚拟机上：    
-      git clone git@code.csdn.net:winlinvip/srs-csdn.git
-	  cd srs-csdn && git checkout master && git branch 1.0release && git push origin 1.0release
+      git clone git@git.oschina.net:winlinvip/srs.oschina.git
+	  cd srs.oschina && git checkout master && git branch 1.0release && git push origin 1.0release
 3. 创建同步的branch：    
       git remote add upstream https://github.com/winlinvip/simple-rtmp-server.git
       git fetch upstream    
       git checkout upstream/master -b srs.master
 	  git checkout upstream/1.0release -b srs.1.0release
 4. 执行本同步更新脚本，更新。
-      bash scripts/csdn.mirror.sh
+      bash scripts/oschina.mirror.sh
 END
     exit 0; 
 fi 
@@ -83,14 +83,14 @@ ok_msg "(1.0release)合并github分支成功"
 for ((;;)); do 
     git push
     ret=$?; if [[ 0 -ne $ret ]]; then 
-        failed_msg "提交CSDN分支失败，自动重试";
+        failed_msg "提交OSChina分支失败，自动重试";
         continue
     else
-        ok_msg "提交CSDN分支成功"
+        ok_msg "提交OSChina分支成功"
     fi
     break
 done
 
-ok_msg "CSDN同步git成功"
+ok_msg "OSChina同步git成功"
 
 exit 0
