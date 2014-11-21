bb7a19636a6efb835e3cf810b5e0c397f54ee5fe
winlin <winlin@vip.126.com>
2014-11-21 14:30:01 +0800

    fix the bandwidth file name bug.

--- trunk/research/librtmp/srs_bandwidth_check.c
+++ trunk/research/librtmp/srs_bandwidth_check.c
@@ -21,7 +21,7 @@ IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 /**
-gcc srs_rtmp_bandwidth_check.c ../../objs/lib/srs_librtmp.a -g -O0 -lstdc++ -o srs_rtmp_bandwidth_check
+gcc srs_bandwidth_check.c ../../objs/lib/srs_librtmp.a -g -O0 -lstdc++ -o srs_bandwidth_check
 */
 
 #include <stdio.h>
