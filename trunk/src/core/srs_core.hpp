6c833902f9cd67f1359b524391df463021407274
winlin <winlin@centos6.6>
2015-01-08 16:02:33 +0800

    merge http api from tufang. 2.0.92

--- trunk/src/core/srs_core.hpp
+++ trunk/src/core/srs_core.hpp
@@ -31,7 +31,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 // current release version
 #define VERSION_MAJOR       2
 #define VERSION_MINOR       0
-#define VERSION_REVISION    91
+#define VERSION_REVISION    92
 // server info.
 #define RTMP_SIG_SRS_KEY "SRS"
 #define RTMP_SIG_SRS_ROLE "origin/edge server"
