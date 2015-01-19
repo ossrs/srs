e6549b261c63858e984be7354c57fd3cab6b4854
winlin <winlin@vip.126.com>
2015-01-19 10:18:18 +0800

    for #293, support http mp3 streaming

---
+++ trunk/src/kernel/srs_kernel_mp3.cpp
@@ -0,0 +1,127 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2013-2015 winlin
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#include <srs_kernel_mp3.hpp>
+
+// for srs-librtmp, @see https://github.com/winlinvip/simple-rtmp-server/issues/213
+#ifndef _WIN32
+#include <unistd.h>
+#endif
+
+#include <fcntl.h>
+#include <sstream>
+using namespace std;
+
+#include <srs_kernel_log.hpp>
+#include <srs_kernel_error.hpp>
+#include <srs_kernel_stream.hpp>
+#include <srs_kernel_file.hpp>
+#include <srs_kernel_codec.hpp>
+
+SrsMp3Encoder::SrsMp3Encoder()
+{
+    _fs = NULL;
+    tag_stream = new SrsStream();
+}
+
+SrsMp3Encoder::~SrsMp3Encoder()
+{
+    srs_freep(tag_stream);
+}
+
+int SrsMp3Encoder::initialize(SrsFileWriter* fs)
+{
+    int ret = ERROR_SUCCESS;
+    
+    srs_assert(fs);
+    
+    if (!fs->is_open()) {
+        ret = ERROR_KERNEL_MP3_STREAM_CLOSED;
+        srs_warn("stream is not open for encoder. ret=%d", ret);
+        return ret;
+    }
+    
+    _fs = fs;
+    
+    return ret;
+}
+
+int SrsMp3Encoder::write_header()
+{
+    static char id3[] = {
+        (char)0x49, (char)0x44, (char)0x33, // ID3
+        (char)0x03, (char)0x00, // version
+        (char)0x00, // flags
+        (char)0x00, (char)0x00, (char)0x00, (char)0x0a, // size
+        
+        (char)0x00, (char)0x00, (char)0x00, (char)0x00, // FrameID
+        (char)0x00, (char)0x00, (char)0x00, (char)0x00, // FrameSize
+        (char)0x00, (char)0x00 // Flags
+    };
+    return _fs->write(id3, sizeof(id3), NULL);
+}
+
+int SrsMp3Encoder::write_audio(int64_t timestamp, char* data, int size)
+{
+    int ret = ERROR_SUCCESS;
+    
+    srs_assert(data);
+    
+    timestamp &= 0x7fffffff;
+    
+    SrsStream* stream = tag_stream;
+    if ((ret = stream->initialize(data, size)) != ERROR_SUCCESS) {
+        return ret;
+    }
+
+    // audio decode
+    if (!stream->require(1)) {
+        ret = ERROR_MP3_DECODE_ERROR;
+        srs_error("mp3 decode audio sound_format failed. ret=%d", ret);
+        return ret;
+    }
+    
+    // @see: E.4.2 Audio Tags, video_file_format_spec_v10_1.pdf, page 76
+    int8_t sound_format = stream->read_1bytes();
+    
+    // @see: SrsAvcAacCodec::audio_aac_demux
+    //int8_t sound_type = sound_format & 0x01;
+    //int8_t sound_size = (sound_format >> 1) & 0x01;
+    //int8_t sound_rate = (sound_format >> 2) & 0x03;
+    sound_format = (sound_format >> 4) & 0x0f;
+    
+    if ((SrsCodecAudio)sound_format != SrsCodecAudioMP3) {
+        ret = ERROR_MP3_DECODE_ERROR;
+        srs_error("mp3 required, format=%d. ret=%d", sound_format, ret);
+        return ret;
+    }
+
+    if (!stream->require(1)) {
+        ret = ERROR_MP3_DECODE_ERROR;
+        srs_error("mp3 decode aac_packet_type failed. ret=%d", ret);
+        return ret;
+    }
+    
+    return _fs->write(data + stream->pos(), size - stream->pos(), NULL);
+}
+
