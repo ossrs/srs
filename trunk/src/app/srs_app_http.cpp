8ee0826e9a11eccd031fcd253a48476dbf448ef8
winlin <winlin@vip.126.com>
2015-03-16 10:24:19 +0800

    fix build failed bug.

--- trunk/src/app/srs_app_http.cpp
+++ trunk/src/app/srs_app_http.cpp
@@ -27,6 +27,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #include <stdlib.h>
 #include <sys/stat.h>
+#include <algorithm>
 using namespace std;
 
 #include <srs_kernel_error.hpp>
