b23e196f20dc7b7fe116d1e029ac7bf29af9abeb
winlin <winlin@vip.126.com>
2015-04-29 17:38:23 +0800

    migarate from winlinvip to srs org, move the copyright to srs org.

--- trunk/research/code-statistic/cs.py
+++ trunk/research/code-statistic/cs.py
@@ -2,7 +2,7 @@
 '''
 The MIT License (MIT)
 
-Copyright (c) 2013-2015 winlin
+Copyright (c) 2013-2015 SRS(simple-rtmp-server)
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of
 this software and associated documentation files (the "Software"), to deal in
