7e04975b8dcc921eaf07c8d59f4e07f1ee12251b
winlin <winlin@vip.126.com>
2015-05-27 11:38:10 +0800

    fix the test configure script

--- trunk/scripts/test_configure.sh
+++ trunk/scripts/test_configure.sh
@@ -272,13 +272,4 @@ ok_msg "test \" ${item} \""
 ret=$?; if [[ $ret -ne 0 ]]; then failed_msg "test \" ${item} \" failed. ret=$ret"; exit $ret; fi
 ok_msg "test \" ${item} \" success"
 
-#######################################################################################################
-#######################################################################################################
-#######################################################################################################
-item="./configure --without-utest"
-ok_msg "test \" ${item} \""
-($item && make $params) >>$log 2>&1
-ret=$?; if [[ $ret -ne 0 ]]; then failed_msg "test \" ${item} \" failed. ret=$ret"; exit $ret; fi
-ok_msg "test \" ${item} \" success"
-
 echo "success"
