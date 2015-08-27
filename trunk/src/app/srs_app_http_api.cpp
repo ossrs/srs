e03d8a0115e964b77fc6e4c85334e36f99ec08c5
winlin <winlin@vip.126.com>
2015-08-28 00:01:19 +0800

    fix the build failed bug on centos

--- trunk/src/app/srs_app_http_api.cpp
+++ trunk/src/app/srs_app_http_api.cpp
@@ -27,6 +27,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #include <sstream>
 #include <stdlib.h>
+#include <signal.h>
 using namespace std;
 
 #include <srs_kernel_log.hpp>
