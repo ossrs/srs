faf6d158782ee331572e648759436ee3f2310f26
winlin <winlin@vip.126.com>
2015-09-21 18:32:17 +0800

    add replace script.

---
+++ trunk/scripts/sources_replace.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+from=$1
+to=$2
+
+if [[ $from == '' ]]; then
+    echo "replace from must not be empty"
+    exit 1
+fi
+
+if [[ $to == '' ]]; then
+    echo "replace to must not be empty"
+    exit 1
+fi
+
+echo "from=$from"
+echo "to=$to"
+
+files="configure `ls auto` `ls conf` `ls scripts` `find etc -type f` `find ide -type f` `find research -type f` `find src -type f`"
+for file in $files; do 
+    grep -in "$from" $file >/dev/null 2>&1; 
+    if [[ 0 -eq $? ]]; then 
+        echo "replace $file"; 
+        #sed -i '' "s|$from|$to|g" $file; 
+    fi; 
+done
