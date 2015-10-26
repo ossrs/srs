dd2945add16924d469539b9fca2042e4dacf956e
winlin <winlin@vip.126.com>
2015-10-26 14:53:54 +0800

    update htbt script

--- trunk/scripts/htbt.sh
+++ trunk/scripts/htbt.sh
@@ -3,5 +3,5 @@ ip=`ifconfig 2>&1|grep "inet addr"|grep -v "127"|awk '{print $2}'|awk -F ':' '{p
 for ((;;)); do
 	echo "heatbeat at `date`"
 	curl 'http://ossrs.net:8085/api/v1/servers' -H 'Content-Type: text/html' --data-binary "{\"ip\":\"${ip}\",\"device_id\":\"respberry-pi2\"}" && echo ""
-	sleep 30
+	sleep 10
 done
