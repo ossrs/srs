e2865c6ac173aac7eb80d7db8d0b055c7681a4cc
winlin <winlin@vip.126.com>
2016-09-01 16:51:09 +0800

    workaround for #511, the fly stfd in close. 2.0.211

--- trunk/src/app/srs_app_st.cpp
+++ trunk/src/app/srs_app_st.cpp
@@ -234,13 +234,9 @@ int srs_st_init()
 void srs_close_stfd(st_netfd_t& stfd)
 {
     if (stfd) {
-        int fd = st_netfd_fileno(stfd);
-        st_netfd_close(stfd);
-        stfd = NULL;
-        
-        // st does not close it sometimes, 
-        // close it manually.
-        close(fd);
+        // we must ensure the close is ok.
+        int err = st_netfd_close(stfd);
+        srs_assert(err != -1);
     }
 }
 
