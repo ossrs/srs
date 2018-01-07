ca1395a8071173f50b2cb6e9482cb6a7570ae906
winlin <winlin@vip.126.com>
2018-01-07 10:58:53 +0800

    Change date from 2017 to 2018

--- trunk/src/protocol/srs_http_stack.cpp
+++ trunk/src/protocol/srs_http_stack.cpp
@@ -1,7 +1,7 @@
 /**
  * The MIT License (MIT)
  *
- * Copyright (c) 2013-2017 OSSRS(winlin)
+ * Copyright (c) 2013-2018 Winlin
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
@@ -3000,7 +3000,7 @@ http_body_is_final(const struct http_parser *parser) {
 /*
  The MIT License (MIT)
  
- Copyright (c) 2013-2017 OSSRS(winlin)
+ Copyright (c) 2013-2018 Winlin
  
  Permission is hereby granted, free of charge, to any person obtaining a copy of
  this software and associated documentation files (the "Software"), to deal in
--- trunk/src/protocol/srs_protocol_json.cpp
+++ trunk/src/protocol/srs_protocol_json.cpp
@@ -1,7 +1,7 @@
 /**
  * The MIT License (MIT)
  *
- * Copyright (c) 2013-2017 OSSRS(winlin)
+ * Copyright (c) 2013-2018 Winlin
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
@@ -1318,7 +1318,7 @@ void json_value_free (json_value * value)
 /**
  * The MIT License (MIT)
  *
- * Copyright (c) 2013-2017 OSSRS(winlin)
+ * Copyright (c) 2013-2018 Winlin
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
