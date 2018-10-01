fcb1a9aea900cd187f7c4ef2791ac2a3a82e2a11
Mirko Velic <mvelic@inoxx.net>
2018-10-01 19:47:23 +1000

    Add request stream param to encoder output format parameters.

--- trunk/src/app/srs_app_encoder.cpp
+++ trunk/src/app/srs_app_encoder.cpp
@@ -285,6 +285,7 @@ int SrsEncoder::initialize_ffmpeg(SrsFFMPEG* ffmpeg, SrsRequest* req, SrsConfDir
     output = srs_string_replace(output, "[port]", req->port);
     output = srs_string_replace(output, "[app]", req->app);
     output = srs_string_replace(output, "[stream]", req->stream);
+    output = srs_string_replace(output, "[param]", req->param);
     output = srs_string_replace(output, "[engine]", engine->arg0());
     
     std::string log_file = SRS_CONSTS_NULL_FILE; // disabled
