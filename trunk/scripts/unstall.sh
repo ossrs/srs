7af087652f969b504d59d93c07874198e68c2a7b
winlin <winlin@vip.126.com>
2019-10-05 20:43:04 +0800

    Add uninstall script

--- trunk/scripts/unstall.sh
+++ trunk/scripts/unstall.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-systemctl disable srs
-systemctl stop srs
+if [[ -d /usr/lib/systemd/system ]]; then
+  systemctl disable srs
+  systemctl stop srs
+  rm -f /usr/lib/systemd/system/srs.service
+  rm -f /etc/init.d/srs
+else
+  /sbin/chkconfig srs off
+  /sbin/chkconfig --del srs
+  /etc/init.d/srs stop
+  rm -f /etc/init.d/srs
+fi
 rm -rf /usr/local/srs
-rm -f /etc/init.d/srs
 echo "SRS uninstalled"
