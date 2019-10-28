cf9a6b4f48220eaaf08f5274e3438aae327b254a
winlin <winlin@vip.126.com>
2019-10-28 08:41:49 +0800

    Cover protocol stack RTMP. 3.0.63

--- trunk/src/utest/srs_utest_protostack.cpp
+++ trunk/src/utest/srs_utest_protostack.cpp
@@ -22,7 +22,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 #include <srs_utest_protostack.hpp>
 
-using namespace std;
+#include <srs_utest_protocol.hpp>
 
 #include <srs_kernel_error.hpp>
 #include <srs_core_autofree.hpp>
@@ -35,6 +35,8 @@ using namespace std;
 #include <srs_rtmp_stack.hpp>
 #include <srs_service_http_conn.hpp>
 #include <srs_kernel_buffer.hpp>
+
+using namespace std;
 
 class MockErrorPacket : public SrsPacket
 {
@@ -72,6 +74,78 @@ VOID TEST(ProtoStackTest, PacketEncode)
     if (true) {
         MockErrorPacket pkt;
         EXPECT_EQ(1024, pkt.get_size());
+    }
+}
+
+VOID TEST(ProtoStackTest, ManualFlush)
+{
+    srs_error_t err;
+
+    if (true) {
+        MockBufferIO io;
+        SrsProtocol p(&io);
+
+        // Always response ACK message.
+        HELPER_EXPECT_SUCCESS(p.set_in_window_ack_size(1));
+
+        // Default is auto response.
+        HELPER_EXPECT_SUCCESS(p.response_acknowledgement_message());
+        EXPECT_EQ(12+4, io.out_buffer.length());
+    }
+
+    if (true) {
+        MockBufferIO io;
+        SrsProtocol p(&io);
+
+        // Always response ACK message.
+        HELPER_EXPECT_SUCCESS(p.set_in_window_ack_size(1));
+
+        p.set_auto_response(true);
+        HELPER_EXPECT_SUCCESS(p.response_acknowledgement_message());
+        EXPECT_EQ(12+4, io.out_buffer.length());
+    }
+
+    if (true) {
+        MockBufferIO io;
+        SrsProtocol p(&io);
+
+        // Always response ACK message.
+        HELPER_EXPECT_SUCCESS(p.set_in_window_ack_size(1));
+
+        // When not auto response, need to flush it manually.
+        p.set_auto_response(false);
+        HELPER_EXPECT_SUCCESS(p.response_acknowledgement_message());
+        EXPECT_EQ(0, io.out_buffer.length());
+
+        HELPER_EXPECT_SUCCESS(p.manual_response_flush());
+        EXPECT_EQ(12+4, io.out_buffer.length());
+    }
+
+    if (true) {
+        MockBufferIO io;
+        SrsProtocol p(&io);
+
+        // Always response ACK message.
+        HELPER_EXPECT_SUCCESS(p.set_in_window_ack_size(1));
+
+        HELPER_EXPECT_SUCCESS(p.response_ping_message(1024));
+        EXPECT_EQ(12+6, io.out_buffer.length());
+    }
+
+    if (true) {
+        MockBufferIO io;
+        SrsProtocol p(&io);
+
+        // Always response ACK message.
+        HELPER_EXPECT_SUCCESS(p.set_in_window_ack_size(1));
+
+        // When not auto response, need to flush it manually.
+        p.set_auto_response(false);
+        HELPER_EXPECT_SUCCESS(p.response_ping_message(1024));
+        EXPECT_EQ(0, io.out_buffer.length());
+
+        HELPER_EXPECT_SUCCESS(p.manual_response_flush());
+        EXPECT_EQ(12+6, io.out_buffer.length());
     }
 }
 
