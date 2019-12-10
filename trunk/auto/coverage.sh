94adda5daec891f27fd57f4273ca0063a0b8211e
winlin <winlin@vip.126.com>
2019-12-10 21:14:01 +0800

    Fix the circleci path

--- trunk/auto/coverage.sh
+++ trunk/auto/coverage.sh
@@ -6,20 +6,24 @@
 #       ./objs/srs_utest
 
 # Workdir is objs/cover.
-mkdir -p objs/cover && cd objs/cover
+workdir=`pwd`/objs/cover
+
+# Create trunk under workdir.
+mkdir -p $workdir/trunk && cd $workdir/trunk
 ret=$?; if [[ $ret -ne 0 ]]; then echo "Enter workdir failed, ret=$ret"; exit $ret; fi
 
 # Collect all *.gcno and *.gcda to objs/cover.
-(rm -rf src && cp -R ../../src . && cp -R ../src .)
+(rm -rf src && cp -R ../../../src . && cp -R ../../src .)
 ret=$?; if [[ $ret -ne 0 ]]; then echo "Collect *.gcno and *.gcda failed, ret=$ret"; exit $ret; fi
 
 # Generate *.gcov for coverage.
-for file in `find src -name "*.cpp"`; do
+for file in `find src -name "*.cpp"|grep -v utest`; do
     gcov $file -o `dirname $file`
     ret=$?; if [[ $ret -ne 0 ]]; then echo "Collect $file failed, ret=$ret"; exit $ret; fi
 done
 
 # Upload report with *.gcov
+cd $workdir/trunk &&
 export CODECOV_TOKEN="493bba46-c468-4e73-8b45-8cdd8ff62d96" &&
 bash <(curl -s https://codecov.io/bash)
 exit 0
