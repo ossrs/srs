995bd5cdd204dc5cbf65c16fe8847e9ddea70597
winlin <winlin@vip.126.com>
2019-12-24 17:29:30 +0800

    Update new AUTHORS

---
+++ trunk/scripts/new_authors.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+AFILE=`dirname $0`/../../AUTHORS.txt
+if [[ ! -f $AFILE ]]; then echo "No file at $AFILE"; exit -1; fi
+
+authors=`git log --format='%ae'|grep -v localhost|grep -v demo|grep -v none|sort|uniq`
+if [[ $? -ne 0 ]]; then echo "no authors"; exit -1; fi
+
+for author in $authors; do
+  echo $author| grep 'winlin' >/dev/null 2>&1 && continue;
+  echo $author| grep 'winterserver' >/dev/null 2>&1 && continue;
+  echo $author| grep 'wenjie.zhao' >/dev/null 2>&1 && continue;
+  echo $author| grep 'zhaowenjie' >/dev/null 2>&1 && continue;
+
+  grep $author $AFILE 1>/dev/null 2>/dev/null && continue;
+
+  git log -1 --author="$author" --format='%an<%ae>'| sed 's/ //g'
+done
