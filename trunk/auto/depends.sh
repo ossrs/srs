c91ffbee934fb30f6aa09a943536c4ec335efca5
winlin <winlin@vip.126.com>
2019-12-25 15:49:25 +0800

    For #1537, #1282, remove not used patches, note the patch for ARM already merged to ST in SRS3

--- trunk/auto/depends.sh
+++ trunk/auto/depends.sh
@@ -436,7 +436,6 @@ if [ $SRS_EXPORT_LIBRTMP_PROJECT = NO ]; then
         (
             rm -rf ${SRS_OBJS}/state-threads-1.9.1 && cd ${SRS_OBJS} &&
             tar xf ../3rdparty/state-threads-1.9.1.tar.gz && cd state-threads-1.9.1 && chmod +w * &&
-            patch -p0 < ../../3rdparty/patches/6.st.osx10.14.build.patch &&
             make ${_ST_MAKE} EXTRA_CFLAGS="${_ST_EXTRA_CFLAGS}" &&
             cd .. && rm -f st && ln -sf state-threads-1.9.1/obj st &&
             rm -f state-threads && ln -sf state-threads-1.9.1 state-threads
