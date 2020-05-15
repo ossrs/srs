36feadf09b6a3908510dbcb4bfab1e2d9afac470
施维 <shiwei9@staff.sina.com.cn>
2020-05-15 16:16:26 +0800

    add hevc in hls

--- trunk/src/app/srs_app_hls.cpp
+++ trunk/src/app/srs_app_hls.cpp
@@ -382,6 +382,8 @@ srs_error_t SrsHlsMuxer::segment_open()
         std::string default_vcodec_str = _srs_config->get_hls_vcodec(req->vhost);
         if (default_vcodec_str == "h264") {
             default_vcodec = SrsVideoCodecIdAVC;
+        } else if (default_vcodec_str == "h265") {
+            default_vcodec = SrsVideoCodecIdHEVC;
         } else if (default_vcodec_str == "vn") {
             default_vcodec = SrsVideoCodecIdDisabled;
         } else {
@@ -1322,7 +1324,7 @@ srs_error_t SrsHls::on_video(SrsSharedPtrMessage* shared_video, SrsFormat* forma
     }
     
     srs_assert(format->vcodec);
-    if (format->vcodec->id != SrsVideoCodecIdAVC) {
+    if ((format->vcodec->id != SrsVideoCodecIdAVC) && (format->vcodec->id != SrsVideoCodecIdHEVC)) {
         return err;
     }
     
