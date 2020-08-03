0905558d49494b4b2687adbdad4890ad04a71ebc
winlin <winlin@vip.126.com>
2020-08-03 19:28:52 +0800

    BUILD: Compatible with 3.0 options, ignore it

--- trunk/auto/options.sh
+++ trunk/auto/options.sh
@@ -488,24 +488,24 @@ function apply_detail_options() {
     
     # if specified export single file, export project first.
     if [ $SRS_EXPORT_LIBRTMP_SINGLE != NO ]; then
-        echo "Not support --export-librtmp-single"
-        exit -1
+        echo "Warning: Ingore --export-librtmp-single"
+        SRS_EXPORT_LIBRTMP_SINGLE=NO
     fi
 
     # disable almost all features for export srs-librtmp.
     if [ $SRS_EXPORT_LIBRTMP_PROJECT != NO ]; then
-        echo "Not support --export-librtmp-project"
-        exit -1
+        echo "Warning: Ingore --export-librtmp-project"
+        SRS_EXPORT_LIBRTMP_PROJECT=NO
     fi
 
     if [[ $SRS_LIBRTMP != NO ]]; then
-        echo "Not support --librtmp"
-        exit -1
+        echo "Warning: Ingore --librtmp"
+        SRS_LIBRTMP=NO
     fi
 
     if [[ $SRS_RESEARCH != NO ]]; then
-        echo "Not support --research"
-        exit -1
+        echo "Warning: Ingore --research"
+        SRS_RESEARCH=NO
     fi
 
     if [[ $SRS_SRTP_ASM == YES && $SRS_RTC == NO ]]; then
