9f9e3a5d42573175168caed9a699bd6a5e14dc8e
winlin <winlin@vip.126.com>
2020-10-31 17:47:27 +0800

    Use global _srs_server for debugging. 3.0.151

--- trunk/src/main/srs_main_server.cpp
+++ trunk/src/main/srs_main_server.cpp
@@ -65,6 +65,9 @@ SrsConfig* _srs_config = new SrsConfig();
 // @global version of srs, which can grep keyword "XCORE"
 extern const char* _srs_version;
 
+// @global main SRS server, for debugging
+SrsServer* _srs_server = NULL;
+
 /**
  * main entrance.
  */
@@ -177,11 +180,9 @@ srs_error_t do_main(int argc, char** argv)
     
     // features
     show_macro_features();
-    
-    SrsServer* svr = new SrsServer();
-    SrsAutoFree(SrsServer, svr);
-    
-    if ((err = run(svr)) != srs_success) {
+
+    _srs_server = new SrsServer();
+    if ((err = run(_srs_server)) != srs_success) {
         return srs_error_wrap(err, "run");
     }
     
