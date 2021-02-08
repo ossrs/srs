154ae9b4ebbbfb388a394cfa585a740534f72485
winlin <winlin@vip.126.com>
2021-02-08 13:16:07 +0800

    RTC: Update stat for twcc and rr

--- trunk/src/app/srs_app_rtc_server.cpp
+++ trunk/src/app/srs_app_rtc_server.cpp
@@ -53,6 +53,13 @@ extern SrsPps* _srs_pps_ids;
 extern SrsPps* _srs_pps_fids;
 extern SrsPps* _srs_pps_fids_level0;
 
+extern SrsPps* _srs_pps_pli;
+extern SrsPps* _srs_pps_twcc;
+extern SrsPps* _srs_pps_rr;
+
+extern SrsPps* _srs_pps_timer;
+extern SrsPps* _srs_pps_pub;
+
 SrsRtcBlackhole::SrsRtcBlackhole()
 {
     blackhole = false;
@@ -225,7 +232,7 @@ SrsRtcServer::SrsRtcServer()
 {
     handler = NULL;
     hijacker = NULL;
-    timer = new SrsHourGlass(this, 1 * SRS_UTIME_SECONDS);
+    timer = new SrsHourGlass("server", this, 1 * SRS_UTIME_SECONDS);
 }
 
 SrsRtcServer::~SrsRtcServer()
@@ -649,12 +656,16 @@ srs_error_t SrsRtcServer::notify(int type, srs_utime_t interval, srs_utime_t tic
     // Update the pps stat for UDP socket and adddresses.
     _srs_pps_pkts->update(); _srs_pps_addrs->update(); _srs_pps_fast_addrs->update();
     _srs_pps_ids->update(); _srs_pps_fids->update(); _srs_pps_fids_level0->update();
+    _srs_pps_pli->update(); _srs_pps_twcc->update(); _srs_pps_rr->update();
+    _srs_pps_timer->update(); _srs_pps_pub->update();
 
     // TODO: FIXME: Show more data for RTC server.
-    srs_trace("RTC: Server conns=%u, cpu=%.2f%%, rss=%dMB, pkts=%d, addrs=%d,%d, fid=%d,%d,%d",
+    srs_trace("RTC: Server conns=%u, cpu=%.2f%%, rss=%dMB, pkts=%d, addrs=%d,%d, fid=%d,%d,%d, rtcp=%d,%d,%d, timer=%d,%d",
         nn_rtc_conns, u->percent * 100, memory,
         _srs_pps_pkts->r10s(), _srs_pps_addrs->r10s(), _srs_pps_fast_addrs->r10s(),
-        _srs_pps_ids->r10s(), _srs_pps_fids->r10s(), _srs_pps_fids_level0->r10s()
+        _srs_pps_ids->r10s(), _srs_pps_fids->r10s(), _srs_pps_fids_level0->r10s(),
+        _srs_pps_pli->r10s(), _srs_pps_twcc->r10s(), _srs_pps_rr->r10s(),
+        _srs_pps_timer->r10s(), _srs_pps_pub->r10s()
     );
 
     return err;
