c0fda4274349ad20d80c23060c9b8012228aa684
winlin <winlin@vip.126.com>
2021-02-11 21:14:45 +0800

    For #2194, Support ST yield

--- trunk/src/protocol/srs_service_st.hpp
+++ trunk/src/protocol/srs_service_st.hpp
@@ -58,6 +58,7 @@ extern srs_error_t srs_fd_keepalive(int fd);
 // Get current coroutine/thread.
 extern srs_thread_t srs_thread_self();
 extern void srs_thread_exit(void* retval);
+extern void srs_thread_yield();
 
 // For client, to open socket and connect to server.
 // @param tm The timeout in srs_utime_t.
