20df644a05638f63da5e5a6444ef09cc89de5eb2
winlin <winlin@vip.126.com>
2021-02-25 13:46:52 +0800

    Kernel: Extract SrsPps to kernel

---
+++ trunk/src/kernel/srs_kernel_kbps.cpp
@@ -0,0 +1,123 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2013-2020 Winlin
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include <srs_kernel_kbps.hpp>
+
+#include <srs_kernel_utility.hpp>
+
+SrsRateSample::SrsRateSample()
+{
+    total = time = -1;
+    rate = 0;
+}
+
+SrsRateSample::~SrsRateSample()
+{
+}
+
+SrsRateSample* SrsRateSample::update(int64_t nn, srs_utime_t t, int k)
+{
+    total = nn;
+    time = t;
+    rate = k;
+    return this;
+}
+
+void srs_pps_init(SrsRateSample& sample, int64_t nn, srs_utime_t now)
+{
+    if (sample.time < 0 || nn < sample.total) {
+        sample.update(nn, now, 0);
+    }
+}
+
+void srs_pps_update(SrsRateSample& sample, int64_t nn, srs_utime_t now)
+{
+    int pps = (int)((nn - sample.total) * 1000 / srsu2ms(now - sample.time));
+    if (pps == 0 && nn > sample.total) {
+        pps = 1; // For pps in (0, 1), we set to 1.
+    }
+    sample.update(nn, now, pps);
+}
+
+SrsPps::SrsPps(SrsWallClock* c)
+{
+    clk_ = c;
+    sugar = 0;
+}
+
+SrsPps::~SrsPps()
+{
+}
+
+void SrsPps::update()
+{
+    update(sugar);
+}
+
+void SrsPps::update(int64_t nn)
+{
+    srs_utime_t now = clk_->now();
+
+    srs_pps_init(sample_10s_, nn, now);
+    srs_pps_init(sample_30s_, nn, now);
+    srs_pps_init(sample_1m_, nn, now);
+    srs_pps_init(sample_5m_, nn, now);
+    srs_pps_init(sample_60m_, nn, now);
+
+    if (now - sample_10s_.time >= 10 * SRS_UTIME_SECONDS) {
+        srs_pps_update(sample_10s_, nn, now);
+    }
+    if (now - sample_30s_.time >= 30 * SRS_UTIME_SECONDS) {
+        srs_pps_update(sample_30s_, nn, now);
+    }
+    if (now - sample_1m_.time >= 60 * SRS_UTIME_SECONDS) {
+        srs_pps_update(sample_1m_, nn, now);
+    }
+    if (now - sample_5m_.time >= 300 * SRS_UTIME_SECONDS) {
+        srs_pps_update(sample_5m_, nn, now);
+    }
+    if (now - sample_60m_.time >= 3600 * SRS_UTIME_SECONDS) {
+        srs_pps_update(sample_60m_, nn, now);
+    }
+}
+
+int SrsPps::r10s()
+{
+    return sample_10s_.rate;
+}
+
+SrsWallClock::SrsWallClock()
+{
+}
+
+SrsWallClock::~SrsWallClock()
+{
+}
+
+srs_utime_t SrsWallClock::now()
+{
+    return srs_get_system_time();
+}
+
+SrsWallClock* _srs_clock = new SrsWallClock();
+
--- trunk/src/protocol/srs_protocol_kbps.hpp
+++ trunk/src/protocol/srs_protocol_kbps.hpp
@@ -27,50 +27,7 @@
 #include <srs_core.hpp>
 
 #include <srs_protocol_io.hpp>
-
-class SrsWallClock;
-
-// A sample for rate-based stat, such as kbps or kps.
-class SrsRateSample
-{
-public:
-    int64_t total;
-    srs_utime_t time;
-    // kbps or kps
-    int rate;
-public:
-    SrsRateSample();
-    virtual ~SrsRateSample();
-public:
-    virtual SrsRateSample* update(int64_t nn, srs_utime_t t, int k);
-};
-
-// A pps manager every some duration.
-class SrsPps
-{
-private:
-    SrsWallClock* clk_;
-private:
-    // samples
-    SrsRateSample sample_10s_;
-    SrsRateSample sample_30s_;
-    SrsRateSample sample_1m_;
-    SrsRateSample sample_5m_;
-    SrsRateSample sample_60m_;
-public:
-    // Sugar for target to stat.
-    int64_t sugar;
-public:
-    SrsPps(SrsWallClock* clk);
-    virtual ~SrsPps();
-public:
-    // Update with the nn which is target.
-    void update();
-    // Update with the nn.
-    void update(int64_t nn);
-    // Get the 10s average stat.
-    int r10s();
-};
+#include <srs_kernel_kbps.hpp>
 
 /**
  * a slice of kbps statistic, for input or output.
@@ -142,21 +99,6 @@ public:
      * resample to generate the value of delta bytes.
      */
     virtual void remark(int64_t* in, int64_t* out) = 0;
-};
-
-/**
- * A time source to provide wall clock.
- */
-class SrsWallClock
-{
-public:
-    SrsWallClock();
-    virtual ~SrsWallClock();
-public:
-    /**
-     * Current time in srs_utime_t.
-     */
-    virtual srs_utime_t now();
 };
 
 /**
@@ -257,8 +199,5 @@ public:
 public:
     virtual int size_memory();
 };
-
-// The global clock.
-extern SrsWallClock* _srs_clock;
 
 #endif
