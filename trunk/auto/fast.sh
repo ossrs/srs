990dc8cda9f6056cfb8a1cddd424bbd04216cdf0
winlin <winlin@vip.126.com>
2021-03-04 17:00:47 +0800

    Script: Fast coverage support module filter

--- trunk/auto/fast.sh
+++ trunk/auto/fast.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+MODULES=""
+if [[ $# -gt 0 ]]; then
+  for module in $@; do
+    module=$(basename $module)
+    if [[ $module == "src" ]]; then
+      MODULES="objs/src"
+      break
+    fi
+    MODULES="$MODULES objs/src/$module"
+  done
+fi
+
+if [[ $MODULES == "" ]]; then
+  MODULES="objs/src"
+fi
+
 IS_LINUX=yes
 uname -s|grep Darwin >/dev/null && IS_DARWIN=yes && IS_LINUX=no
 echo "IS_LINUX: $IS_LINUX, IS_DARWIN: $IS_DARWIN"
@@ -10,8 +26,8 @@ find objs -name *.gcda |xargs rm -f
 echo "Build and run utest"
 make -j10 && ./objs/srs_utest
 
-echo "Generating coverage"
+echo "Generating coverage at $MODULES"
 mkdir -p ./objs/coverage &&
-gcovr -r src --html --html-details -o ./objs/coverage/srs.html objs/src &&
+gcovr -r src --html --html-details -o ./objs/coverage/srs.html $MODULES &&
 echo "Coverage report at ./objs/coverage/srs.html" &&
 open ./objs/coverage/srs.html
