aae0f611652bac09d029c0aba4f9378d24c1fc91
winlin <winlin@vip.126.com>
2021-04-06 10:56:40 +0800

    RTC: Eliminate the dup code

--- trunk/src/app/srs_app_rtc_conn.cpp
+++ trunk/src/app/srs_app_rtc_conn.cpp
@@ -480,7 +480,7 @@ void SrsRtcPlayStream::on_stream_change(SrsRtcStreamDescription* desc)
 
     // Refresh the relation for video.
     // TODO: FIMXE: Match by label?
-    if (desc && desc->video_track_descs_.size() == 1 && desc->video_track_descs_.size() == 1) {
+    if (desc && desc->video_track_descs_.size() == 1) {
         SrsRtcTrackDescription* vdesc = desc->video_track_descs_.at(0);
         uint32_t ssrc = vdesc->ssrc_;
         SrsRtcVideoSendTrack* track = video_tracks_.begin()->second;
