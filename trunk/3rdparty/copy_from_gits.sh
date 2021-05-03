fc23b9e5f53299dbe017db6d7c05a73e59a39a6a
winlin <winlin@vip.126.com>
2021-05-03 10:06:56 +0800

    Tools: Refine script

--- trunk/3rdparty/copy_from_gits.sh
+++ trunk/3rdparty/copy_from_gits.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
+if [[ ! -d signaling ]]; then
+  cd 3rdparty
+fi
+if [[ ! -d signaling ]]; then
+  echo "no 3rdparty"
+  exit -1
+fi
+if [[ ! -d ~/git/signaling ]]; then
+  echo "no signaling"
+  exit -1
+fi
+
 echo "Copy signaling"
-cp -R ~/signaling/* signaling/
+cp -R ~/git/signaling/* signaling/
 
 echo "Copy httpx-static"
-cp -R ~/httpx-static/* httpx-static/
+cp -R ~/git/go-oryx/httpx-static/* httpx-static/
 
 echo "Copy srs-bench"
-cp -R ~/srs-bench/* srs-bench/
+cp -R ~/git/srs-bench/* srs-bench/
