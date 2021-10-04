0e51b93e31b586e016af37b8550f87c0a525585b
louis.xia <68469352@qq.com>
2021-10-04 13:37:51 +0800

    GB28181: Always enable jitter for RTP. (#2647)
    * remove rtp jitbuffer enable/disable, only enable
    
    * remove rtp jitbuffer enable/disable, only enable
    
    Co-authored-by: xialixin@kanzhun.com <xialixin@kanzhun.com>

--- trunk/src/app/srs_app_config.cpp
+++ trunk/src/app/srs_app_config.cpp
@@ -2635,7 +2635,7 @@ srs_error_t SrsConfig::check_normal_config()
             if (n != "enabled" && n != "caster" && n != "output"
                 && n != "listen" && n != "tcp_enable" && n != "rtp_port_min" && n != "rtp_port_max"
                 && n != "rtp_idle_timeout" && n != "sip"
-                && n != "audio_enable" && n != "wait_keyframe" && n != "jitterbuffer_enable"
+                && n != "audio_enable" && n != "wait_keyframe"
                 && n != "host" && n != "auto_create_channel") {
                 return srs_error_new(ERROR_SYSTEM_CONFIG_INVALID, "illegal stream_caster.%s", n.c_str());
             }
@@ -3588,21 +3588,6 @@ bool SrsConfig::get_stream_caster_gb28181_audio_enable(SrsConfDirective* conf)
     return SRS_CONF_PERFER_FALSE(conf->arg0());
 }
 
-bool SrsConfig::get_stream_caster_gb28181_jitterbuffer_enable(SrsConfDirective* conf)
-{
-    static bool DEFAULT = true;
-
-    if (!conf) {
-        return DEFAULT;
-    }
-
-    conf = conf->get("jitterbuffer_enable");
-    if (!conf || conf->arg0().empty()) {
-        return DEFAULT;
-    }
-
-    return SRS_CONF_PERFER_FALSE(conf->arg0());
-}
 
 bool SrsConfig::get_stream_caster_gb28181_wait_keyframe(SrsConfDirective* conf)
 {
