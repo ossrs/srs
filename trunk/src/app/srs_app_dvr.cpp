97b81c62b77b28ee5b6299d80580826698ecb229
Haibo Chen <nmgchenhaibo@foxmail.com>
2021-11-04 19:23:06 +0800

    DVR: copy req from publish

--- trunk/src/app/srs_app_dvr.cpp
+++ trunk/src/app/srs_app_dvr.cpp
@@ -598,8 +598,12 @@ srs_error_t SrsDvrPlan::initialize(SrsOriginHub* h, SrsDvrSegmenter* s, SrsReque
     return err;
 }
 
-srs_error_t SrsDvrPlan::on_publish()
+srs_error_t SrsDvrPlan::on_publish(SrsRequest* r)
 {
+    // @see https://github.com/ossrs/srs/issues/1613#issuecomment-960623359
+    if (req) srs_freep(req);
+    req = r->copy();
+
     return srs_success;
 }
 
@@ -687,11 +691,11 @@ SrsDvrSessionPlan::~SrsDvrSessionPlan()
 {
 }
 
-srs_error_t SrsDvrSessionPlan::on_publish()
+srs_error_t SrsDvrSessionPlan::on_publish(SrsRequest* r)
 {
     srs_error_t err = srs_success;
 
-    if ((err = SrsDvrPlan::on_publish()) != srs_success) {
+    if ((err = SrsDvrPlan::on_publish(r)) != srs_success) {
         return err;
     }
     
@@ -762,11 +766,11 @@ srs_error_t SrsDvrSegmentPlan::initialize(SrsOriginHub* h, SrsDvrSegmenter* s, S
     return srs_success;
 }
 
-srs_error_t SrsDvrSegmentPlan::on_publish()
+srs_error_t SrsDvrSegmentPlan::on_publish(SrsRequest* r)
 {
     srs_error_t err = srs_success;
 
-    if ((err = SrsDvrPlan::on_publish()) != srs_success) {
+    if ((err = SrsDvrPlan::on_publish(r)) != srs_success) {
         return err;
     }
     
@@ -954,7 +958,7 @@ srs_error_t SrsDvr::on_publish()
         return err;
     }
     
-    if ((err = plan->on_publish()) != srs_success) {
+    if ((err = plan->on_publish(req)) != srs_success) {
         return srs_error_wrap(err, "publish");
     }
     
