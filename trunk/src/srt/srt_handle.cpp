6b9eef265a524444692561c27cca10f9c8b3923f
zhouxiaojun <279686030@qq.com>
2022-02-14 09:20:11 +0800

    SRT: using global variables  to pass errors (#2897)

--- trunk/src/srt/srt_handle.cpp
+++ trunk/src/srt/srt_handle.cpp
@@ -206,6 +206,18 @@ void srt_handle::handle_push_data(SRT_SOCKSTATUS status, const std::string& subp
     srt_conn_ptr->update_timestamp(srt_now_ms);
 
     srt2rtmp::get_instance()->insert_data_message(data, ret, subpath);
+    {
+        std::unique_lock<std::mutex> locker(srt2rtmp::_srt_error_mutex);
+        if (srt2rtmp::_srt_error_map.count(subpath) == 1) {
+            if (srt2rtmp::_srt_error_map[subpath] != 0) {
+                close_push_conn(conn_fd);
+                srt_log_error("handle_push_data srt to rtmp error, fd:%d", conn_fd);                
+                //todo: reset to next use, maybe update by srt2rtmp::cycle again
+                srt2rtmp::_srt_error_map[subpath] = 0; 
+                return;
+            }
+        }
+    }
     
     //send data to subscriber(players)
     //streamid, play map<SRTSOCKET, SRT_CONN_PTR>
