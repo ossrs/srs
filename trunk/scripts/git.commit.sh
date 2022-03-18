06cff31b24eac51c203a19c33542fe5baa7f3ab4
winlin <winlin@vip.126.com>
2022-03-18 12:02:49 +0800

    Update script.

--- trunk/scripts/git.commit.sh
+++ trunk/scripts/git.commit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for file in $(git remote -v|grep -v https|grep -v gb28181|grep push|awk '{print $1}'); do 
+for file in $(git remote -v|grep -v https|grep -v gb28181|grep -v tmp|grep push|awk '{print $1}'); do 
     echo ""; 
     echo "git push $file $@"; 
     git push $file $@; 
