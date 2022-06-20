d117145b9576292060387c18e0502c47a987d158
winlin <winlin@vip.126.com>
2022-06-20 19:22:25 +0800

    Update date from 2021 to 2022.

--- trunk/src/app/srs_app_pithy_print.hpp
+++ trunk/src/app/srs_app_pithy_print.hpp
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2013-2021 The SRS Authors
+// Copyright (c) 2013-2022 The SRS Authors
 //
 // SPDX-License-Identifier: MIT or MulanPSL-2.0
 //
--- trunk/src/protocol/srs_protocol_json.cpp
+++ trunk/src/protocol/srs_protocol_json.cpp
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2013-2021 The SRS Authors
+// Copyright (c) 2013-2022 The SRS Authors
 //
 // SPDX-License-Identifier: MIT or MulanPSL-2.0
 //
@@ -1301,7 +1301,7 @@ void json_value_free (json_value * value)
 // LCOV_EXCL_STOP
 
 //
-// Copyright (c) 2013-2021 The SRS Authors
+// Copyright (c) 2013-2022 The SRS Authors
 //
 // SPDX-License-Identifier: MIT or MulanPSL-2.0
 //
