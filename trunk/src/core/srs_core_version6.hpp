4b3fe689730c80877be02e3e12e744360869e632
Winlin <winlin@vip.126.com>
2022-11-22 22:20:19 +0800

    Init SRS 6. v6.0.0 (#3269)

---
+++ trunk/src/core/srs_core_version6.hpp
@@ -0,0 +1,14 @@
+//
+// Copyright (c) 2013-2022 The SRS Authors
+//
+// SPDX-License-Identifier: MIT or MulanPSL-2.0
+//
+
+#ifndef SRS_CORE_VERSION6_HPP
+#define SRS_CORE_VERSION6_HPP
+
+#define VERSION_MAJOR       6
+#define VERSION_MINOR       0
+#define VERSION_REVISION    0
+
+#endif
