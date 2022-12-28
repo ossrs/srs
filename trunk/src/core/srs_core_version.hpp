351f7590dbee24734623a7ef6fb951a8817550db
winlin <winlin@vip.126.com>
2022-12-28 14:20:18 +0800

    SRS5: Script: Discover version from code.
    PICK 87a2ef100adbcc14c207a20c5d0ce7ce8bd9f198

---
+++ trunk/src/core/srs_core_version.hpp
@@ -0,0 +1,12 @@
+//
+// Copyright (c) 2013-2022 The SRS Authors
+//
+// SPDX-License-Identifier: MIT or MulanPSL-2.0
+//
+
+#ifndef SRS_CORE_VERSION_HPP
+#define SRS_CORE_VERSION_HPP
+
+#include <srs_core_version6.hpp>
+
+#endif
