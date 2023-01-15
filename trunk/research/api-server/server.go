2b6384426678340775c6bb39f206ba49253e8beb
winlin <winlin@vip.126.com>
2023-01-15 13:44:31 +0800

    Refine streams, sessions, dvrs and hls API.

--- trunk/research/api-server/server.go
+++ trunk/research/api-server/server.go
@@ -8,7 +8,6 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
-	"net/url"
 	"os"
 	"os/exec"
 	"path"
@@ -25,16 +24,6 @@ const (
 	error_system_parse_json = 101
 	// request action invalid
 	error_request_invalid_action = 200
-
-	// chat id not exist
-	error_chat_id_not_exist = 300
-
-	session_action_on_play = "on_play"
-	session_action_on_stop = "on_stop"
-)
-
-const (
-	HttpJson = "application/json"
 )
 
 type SrsError struct {
@@ -45,7 +34,7 @@ type SrsError struct {
 func Response(se *SrsError) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		body, _ := json.Marshal(se)
-		w.Header().Set("Content-Type", HttpJson)
+		w.Header().Set("Content-Type", "application/json")
 		w.Write(body)
 	})
 }
@@ -67,7 +56,7 @@ func WriteDataResponse(w http.ResponseWriter, data interface{}) {
 		return
 	}
 
-	w.Header().Set("Content-Type", HttpJson)
+	w.Header().Set("Content-Type", "application/json")
 	w.Write(j)
 }
 
@@ -79,7 +68,6 @@ See also: https://github.com/ossrs/srs
 `
 
 var StaticDir string
-var cm *ChatManager
 var sw *SnapshotWorker
 
 // All
@@ -138,7 +126,7 @@ type ClientMsg struct {
 
 func (v *ClientMsg) Parse(b []byte) error {
 	if err := json.Unmarshal(b, v); err != nil {
-		return fmt.Errorf("parse action from %v, err %v", string(b), err)
+		return fmt.Errorf("parse message from %v, err %v", string(b), err)
 	}
 	return nil
 }
@@ -186,47 +174,24 @@ func (v *ClientMsg) String() string {
 */
 type StreamMsg struct {
 	CommonRequest
-	Stream   string `json:"stream"`
-	Param    string `json:"param"`
+	Stream string `json:"stream"`
+	Param  string `json:"param"`
 }
 
-func (v *StreamMsg) String() string {
-	return fmt.Sprintf("srs %v: client id=%v, ip=%v, vhost=%v, app=%v, stream=%v, param=%v", v.Action, v.ClientId, v.Ip, v.Vhost, v.App, v.Stream, v.Param)
-}
-
-type Stream struct{}
-
-func (v *Stream) Parse(body []byte) (se *SrsError) {
-	msg := &StreamMsg{}
-	if err := json.Unmarshal(body, msg); err != nil {
-		return &SrsError{Code: error_system_parse_json, Data: fmt.Sprintf("parse stream msg failed, err is %v", err.Error())}
+func (v *StreamMsg) Parse(b []byte) error {
+	if err := json.Unmarshal(b, v); err != nil {
+		return fmt.Errorf("parse message from %v, err %v", string(b), err)
 	}
-	log.Println(msg)
 	return nil
 }
 
-// handle the streams requests: publish/unpublish stream.
-
-func StreamServe(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "GET" {
-		res := struct{}{}
-		body, _ := json.Marshal(res)
-		w.Write(body)
-	} else if r.Method == "POST" {
-		body, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			Response(&SrsError{Code: error_system_read_request, Data: fmt.Sprintf("read request body failed, err is %v", err)}).ServeHTTP(w, r)
-			return
-		}
-		log.Println(fmt.Sprintf("post to streams, req=%v", string(body)))
-		c := &Stream{}
-		if se := c.Parse(body); se != nil {
-			Response(se).ServeHTTP(w, r)
-			return
-		}
-		Response(&SrsError{Code: 0, Data: nil}).ServeHTTP(w, r)
-		return
+func (v *StreamMsg) String() string {
+	var sb strings.Builder
+	sb.WriteString(v.CommonRequest.String())
+	if v.Action == "on_publish" || v.Action == "on_unpublish" {
+		sb.WriteString(fmt.Sprintf(", stream=%v, param=%v", v.Stream, v.Param))
 	}
+	return sb.String()
 }
 
 /*
@@ -263,74 +228,29 @@ func StreamServe(w http.ResponseWriter, r *http.Request) {
 
 type SessionMsg struct {
 	CommonRequest
-	Stream   string `json:"stream"`
-	Param    string `json:"param"`
-}
-
-type SessionOnPlayMsg struct {
-	SessionMsg
+	Stream string `json:"stream"`
+	Param  string `json:"param"`
+	// For on_play only.
 	PageUrl string `json:"pageUrl"`
 }
 
-func (v *SessionOnPlayMsg) String() string {
-	return fmt.Sprintf("srs %v: client id=%v, ip=%v, vhost=%v, app=%v, stream=%v, param=%v, pageUrl=%v", v.Action, v.ClientId, v.Ip, v.Vhost, v.App, v.Stream, v.Param, v.PageUrl)
-}
-
-type SessionOnStopMsg struct {
-	SessionMsg
-}
-
-func (v *SessionOnStopMsg) String() string {
-	return fmt.Sprintf("srs %v: client id=%v, ip=%v, vhost=%v, app=%v, stream=%v, param=%v", v.Action, v.ClientId, v.Ip, v.Vhost, v.App, v.Stream, v.Param)
-}
-
-type Session struct{}
-
-func (v *Session) Parse(body []byte) (se *SrsError) {
-	data := &struct {
-		Action string `json:"action"`
-	}{}
-	if err := json.Unmarshal(body, data); err != nil {
-		return &SrsError{Code: error_system_parse_json, Data: fmt.Sprintf("parse session action failed, err is %v", err.Error())}
-	}
-
-	if data.Action == session_action_on_play {
-		msg := &SessionOnPlayMsg{}
-		if err := json.Unmarshal(body, msg); err != nil {
-			return &SrsError{Code: error_system_parse_json, Data: fmt.Sprintf("parse session %v msg failed, err is %v", data.Action, err.Error())}
-		}
-		log.Println(msg)
-	} else if data.Action == session_action_on_stop {
-		msg := &SessionOnStopMsg{}
-		if err := json.Unmarshal(body, msg); err != nil {
-			return &SrsError{Code: error_system_parse_json, Data: fmt.Sprintf("parse session %v msg failed, err is %v", data.Action, err.Error())}
-		}
-		log.Println(msg)
+func (v *SessionMsg) Parse(b []byte) error {
+	if err := json.Unmarshal(b, v); err != nil {
+		return fmt.Errorf("parse message from %v, err %v", string(b), err)
 	}
 	return nil
 }
 
-// handle the sessions requests: client play/stop stream
-func SessionServe(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "GET" {
-		res := struct{}{}
-		body, _ := json.Marshal(res)
-		w.Write(body)
-	} else if r.Method == "POST" {
-		body, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			Response(&SrsError{Code: error_system_read_request, Data: fmt.Sprintf("read request body failed, err is %v", err)}).ServeHTTP(w, r)
-			return
-		}
-		log.Println(fmt.Sprintf("post to sessions, req=%v", string(body)))
-		c := &Session{}
-		if se := c.Parse(body); se != nil {
-			Response(se).ServeHTTP(w, r)
-			return
-		}
-		Response(&SrsError{Code: 0, Data: nil}).ServeHTTP(w, r)
-		return
+func (v *SessionMsg) String() string {
+	var sb strings.Builder
+	sb.WriteString(v.CommonRequest.String())
+	if v.Action == "on_play" || v.Action == "on_stop" {
+		sb.WriteString(fmt.Sprintf(", stream=%v, param=%v", v.Stream, v.Param))
 	}
+	if v.Action == "on_play" {
+		sb.WriteString(fmt.Sprintf(", pageUrl=%v", v.PageUrl))
+	}
+	return sb.String()
 }
 
 /*
@@ -356,48 +276,26 @@ func SessionServe(w http.ResponseWriter, r *http.Request) {
 
 type DvrMsg struct {
 	CommonRequest
-	Stream   string `json:"stream"`
-	Param    string `json:"param"`
-	Cwd      string `json:"cwd"`
-	File     string `json:"file"`
+	Stream string `json:"stream"`
+	Param  string `json:"param"`
+	Cwd    string `json:"cwd"`
+	File   string `json:"file"`
 }
 
-func (v *DvrMsg) String() string {
-	return fmt.Sprintf("srs %v: client id=%v, ip=%v, vhost=%v, app=%v, stream=%v, param=%v, cwd=%v, file=%v", v.Action, v.ClientId, v.Ip, v.Vhost, v.App, v.Stream, v.Param, v.Cwd, v.File)
-}
-
-type Dvr struct{}
-
-func (v *Dvr) Parse(body []byte) (se *SrsError) {
-	msg := &DvrMsg{}
-	if err := json.Unmarshal(body, msg); err != nil {
-		return &SrsError{Code: error_system_parse_json, Data: fmt.Sprintf("parse dvr msg failed, err is %v", err.Error())}
+func (v *DvrMsg) Parse(b []byte) error {
+	if err := json.Unmarshal(b, v); err != nil {
+		return fmt.Errorf("parse message from %v, err %v", string(b), err)
 	}
-	log.Println(msg)
 	return nil
 }
 
-// handle the dvrs requests: dvr stream.
-func DvrServe(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "GET" {
-		res := struct{}{}
-		body, _ := json.Marshal(res)
-		w.Write(body)
-	} else if r.Method == "POST" {
-		body, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			Response(&SrsError{Code: error_system_read_request, Data: fmt.Sprintf("read request body failed, err is %v", err)}).ServeHTTP(w, r)
-			return
-		}
-		log.Println(fmt.Sprintf("post to dvrs, req=%v", string(body)))
-		c := &Dvr{}
-		if se := c.Parse(body); se != nil {
-			Response(se).ServeHTTP(w, r)
-			return
-		}
-		Response(&SrsError{Code: 0, Data: nil}).ServeHTTP(w, r)
-		return
+func (v *DvrMsg) String() string {
+	var sb strings.Builder
+	sb.WriteString(v.CommonRequest.String())
+	if v.Action == "on_dvr" {
+		sb.WriteString(fmt.Sprintf(", stream=%v, param=%v, cwd=%v, file=%v", v.Stream, v.Param, v.Cwd, v.File))
 	}
+	return sb.String()
 }
 
 /*
@@ -417,7 +315,7 @@ func DvrServe(w http.ResponseWriter, r *http.Request) {
         when srs reap a dvr file, call the hook,
         the request in the POST data string is a object encode by json:
               {
-                  "action": "on_dvr",
+                  "action": "on_hls",
                   "client_id": "9308h583",
                   "ip": "192.168.1.10",
                   "vhost": "video.test.com",
@@ -435,11 +333,7 @@ func DvrServe(w http.ResponseWriter, r *http.Request) {
 */
 
 type HlsMsg struct {
-	Action   string  `json:"action"`
-	ClientId string  `json:"client_id"`
-	Ip       string  `json:"ip"`
-	Vhost    string  `json:"vhost"`
-	App      string  `json:"app"`
+	CommonRequest
 	Stream   string  `json:"stream"`
 	Param    string  `json:"param"`
 	Duration float64 `json:"duration"`
@@ -448,162 +342,20 @@ type HlsMsg struct {
 	SeqNo    int     `json:"seq_no"`
 }
 
+func (v *HlsMsg) Parse(b []byte) error {
+	if err := json.Unmarshal(b, v); err != nil {
+		return fmt.Errorf("parse message from %v, err %v", string(b), err)
+	}
+	return nil
+}
+
 func (v *HlsMsg) String() string {
-	return fmt.Sprintf("srs %v: client id=%v, ip=%v, vhost=%v, app=%v, stream=%v, param=%v, duration=%v, cwd=%v, file=%v, seq_no=%v", v.Action, v.ClientId, v.Ip, v.Vhost, v.App, v.Stream, v.Param, v.Duration, v.Cwd, v.File, v.SeqNo)
-}
-
-type Hls struct{}
-
-func (v *Hls) Parse(body []byte) (se *SrsError) {
-	msg := &HlsMsg{}
-	if err := json.Unmarshal(body, msg); err != nil {
-		return &SrsError{Code: error_system_parse_json, Data: fmt.Sprintf("parse hls msg failed, err is %v", err.Error())}
+	var sb strings.Builder
+	sb.WriteString(v.CommonRequest.String())
+	if v.Action == "on_hls" {
+		sb.WriteString(fmt.Sprintf(", stream=%v, param=%v, cwd=%v, file=%v, duration=%v, seq_no=%v", v.Stream, v.Param, v.Cwd, v.File, v.Duration, v.SeqNo))
 	}
-	log.Println(msg)
-	return nil
-}
-
-// handle the hls requests: hls stream.
-func HlsServe(w http.ResponseWriter, r *http.Request) {
-	log.Println(fmt.Sprintf("hls serve, uPath=%v", r.URL.Path))
-	if r.Method == "GET" {
-		subPath := r.URL.Path[len("/api/v1/hls/"):]
-		res := struct {
-			Args   []string   `json:"args"`
-			KwArgs url.Values `json:"kwargs"`
-		}{
-			Args:   strings.Split(subPath, "/"),
-			KwArgs: r.URL.Query(),
-		}
-		body, _ := json.Marshal(res)
-		w.Write(body)
-	} else if r.Method == "POST" {
-		body, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			Response(&SrsError{Code: error_system_read_request, Data: fmt.Sprintf("read request body failed, err is %v", err)}).ServeHTTP(w, r)
-			return
-		}
-		log.Println(fmt.Sprintf("post to hls, req=%v", string(body)))
-		c := &Hls{}
-		if se := c.Parse(body); se != nil {
-			Response(se).ServeHTTP(w, r)
-			return
-		}
-		Response(&SrsError{Code: 0, Data: nil}).ServeHTTP(w, r)
-		return
-	}
-}
-
-/*
-# object fields:
-# id: an int value indicates the id of user.
-# username: a str indicates the user name.
-# url: a str indicates the url of user stream.
-# agent: a str indicates the agent of user.
-# join_date: a number indicates the join timestamp in seconds.
-# join_date_str: a str specifies the formated friendly time.
-# heartbeat: a number indicates the heartbeat timestamp in seconds.
-# vcodec: a dict indicates the video codec info.
-# acodec: a dict indicates the audio codec info.
-
-# dead time in seconds, if exceed, remove the chat.
-*/
-
-type Chat struct {
-	Id          int    `json:"id"`
-	Username    string `json:"username"`
-	Url         string `json:"url"`
-	JoinDate    int64  `json:"join_date"`
-	JoinDateStr string `json:"join_date_str"`
-	Heartbeat   int64  `json:"heartbeat"`
-}
-
-type ChatManager struct {
-	globalId int
-	chats    *sync.Map
-	deadTime int
-}
-
-func NewChatManager() *ChatManager {
-	v := &ChatManager{
-		globalId: 100,
-		// key is globalId, value is chat
-		chats:    new(sync.Map),
-		deadTime: 15,
-	}
-	return v
-}
-
-func (v *ChatManager) List() (chats []*Chat) {
-	chats = []*Chat{}
-	v.chats.Range(func(key, value interface{}) bool {
-		_, chat := key.(int), value.(*Chat)
-		if (time.Now().Unix() - chat.Heartbeat) > int64(v.deadTime) {
-			v.chats.Delete(key)
-			return true
-		}
-		chats = append(chats, chat)
-		return true
-	})
-	return
-}
-
-func (v *ChatManager) Update(id int) (se *SrsError) {
-	value, ok := v.chats.Load(id)
-	if !ok {
-		return &SrsError{Code: error_chat_id_not_exist, Data: fmt.Sprintf("cannot find id:%v", id)}
-	}
-	c := value.(*Chat)
-	c.Heartbeat = time.Now().Unix()
-	log.Println(fmt.Sprintf("heartbeat chat success, id=%v", id))
-	return nil
-}
-
-func (v *ChatManager) Delete(id int) (se *SrsError) {
-	if _, ok := v.chats.Load(id); !ok {
-		return &SrsError{Code: error_chat_id_not_exist, Data: fmt.Sprintf("cannot find id:%v", id)}
-	}
-	v.chats.Delete(id)
-	log.Println(fmt.Sprintf("delete chat success, id=%v", id))
-	return
-}
-
-func (v *ChatManager) Add(c *Chat) {
-	c.Id = v.globalId
-	now := time.Now()
-	c.JoinDate, c.Heartbeat = now.Unix(), now.Unix()
-	c.JoinDateStr = now.Format("2006-01-02 15:04:05")
-	v.globalId += 1
-	v.chats.Store(c.Id, c)
-}
-
-// the chat streams, public chat room.
-func ChatServe(w http.ResponseWriter, r *http.Request) {
-	log.Println(fmt.Sprintf("got a chat req, uPath=%v", r.URL.Path))
-	if r.Method == "GET" {
-		chats := cm.List()
-		Response(&SrsError{Code: 0, Data: chats}).ServeHTTP(w, r)
-	} else if r.Method == "POST" {
-		body, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			Response(&SrsError{Code: error_system_read_request, Data: fmt.Sprintf("read request body failed, err is %v", err)}).ServeHTTP(w, r)
-			return
-		}
-		c := &Chat{}
-		if err := json.Unmarshal(body, c); err != nil {
-			Response(&SrsError{Code: error_system_parse_json, Data: fmt.Sprintf("parse body to chat json failed, err is %v", err)})
-			return
-		}
-		cm.Add(c)
-		log.Println(fmt.Sprintf("create chat success, id=%v", c.Id))
-		Response(&SrsError{Code: 0, Data: nil}).ServeHTTP(w, r)
-	} else if r.Method == "PUT" {
-		// TODO: parse id?
-		Response(cm.Update(0)).ServeHTTP(w, r)
-	} else if r.Method == "DELETE" {
-		// TODO: parse id?
-		Response(cm.Delete(0)).ServeHTTP(w, r)
-	}
+	return sb.String()
 }
 
 /*
@@ -893,7 +645,6 @@ func main() {
 	}
 
 	log.SetFlags(log.Lshortfile | log.Ldate | log.Ltime | log.Lmicroseconds)
-	cm = NewChatManager()
 	sw = NewSnapshotWorker(ffmpegPath)
 	go sw.Serve()
 
@@ -964,14 +715,118 @@ func main() {
 		}
 	})
 
-	http.HandleFunc("/api/v1/streams", StreamServe)
-	http.HandleFunc("/api/v1/sessions", SessionServe)
-	http.HandleFunc("/api/v1/dvrs", DvrServe)
-	http.HandleFunc("/api/v1/hls", HlsServe)
-	http.HandleFunc("/api/v1/hls/", HlsServe)
+	// handle the streams requests: publish/unpublish stream.
+	http.HandleFunc("/api/v1/streams", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "POST" {
+			WriteDataResponse(w, struct{}{})
+			return
+		}
+
+		if err := func() error {
+			body, err := ioutil.ReadAll(r.Body)
+			if err != nil {
+				return fmt.Errorf("read request body, err %v", err)
+			}
+			log.Println(fmt.Sprintf("post to streams, req=%v", string(body)))
+
+			msg := &StreamMsg{}
+			if err := msg.Parse(body); err != nil {
+				return err
+			}
+			log.Println(fmt.Sprintf("Got %v", msg.String()))
+
+			WriteDataResponse(w, &SrsCommonResponse{Code: 0})
+			return nil
+		}(); err != nil {
+			WriteErrorResponse(w, err)
+		}
+	})
+
+	// handle the sessions requests: client play/stop stream
+	http.HandleFunc("/api/v1/sessions", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "POST" {
+			WriteDataResponse(w, struct{}{})
+			return
+		}
+
+		if err := func() error {
+			body, err := ioutil.ReadAll(r.Body)
+			if err != nil {
+				return fmt.Errorf("read request body, err %v", err)
+			}
+			log.Println(fmt.Sprintf("post to sessions, req=%v", string(body)))
+
+			msg := &SessionMsg{}
+			if err := msg.Parse(body); err != nil {
+				return err
+			}
+			log.Println(fmt.Sprintf("Got %v", msg.String()))
+
+			WriteDataResponse(w, &SrsCommonResponse{Code: 0})
+			return nil
+		}(); err != nil {
+			WriteErrorResponse(w, err)
+		}
+	})
+
+	// handle the dvrs requests: dvr stream.
+	http.HandleFunc("/api/v1/dvrs", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "POST" {
+			WriteDataResponse(w, struct{}{})
+			return
+		}
+
+		if err := func() error {
+			body, err := ioutil.ReadAll(r.Body)
+			if err != nil {
+				return fmt.Errorf("read request body, err %v", err)
+			}
+			log.Println(fmt.Sprintf("post to dvrs, req=%v", string(body)))
+
+			msg := &DvrMsg{}
+			if err := msg.Parse(body); err != nil {
+				return err
+			}
+			log.Println(fmt.Sprintf("Got %v", msg.String()))
+
+			WriteDataResponse(w, &SrsCommonResponse{Code: 0})
+			return nil
+		}(); err != nil {
+			WriteErrorResponse(w, err)
+		}
+	})
+
+	// handle the dvrs requests: on_hls stream.
+	http.HandleFunc("/api/v1/hls", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "POST" {
+			WriteDataResponse(w, struct{}{})
+			return
+		}
+
+		if err := func() error {
+			body, err := ioutil.ReadAll(r.Body)
+			if err != nil {
+				return fmt.Errorf("read request body, err %v", err)
+			}
+			log.Println(fmt.Sprintf("post to hls, req=%v", string(body)))
+
+			msg := &HlsMsg{}
+			if err := msg.Parse(body); err != nil {
+				return err
+			}
+			log.Println(fmt.Sprintf("Got %v", msg.String()))
+
+			WriteDataResponse(w, &SrsCommonResponse{Code: 0})
+			return nil
+		}(); err != nil {
+			WriteErrorResponse(w, err)
+		}
+	})
 
 	// not support yet
-	http.HandleFunc("/api/v1/chat", ChatServe)
+	http.HandleFunc("/api/v1/chat", func(w http.ResponseWriter, r *http.Request) {
+		WriteErrorResponse(w, fmt.Errorf("not implemented"))
+	})
 
 	http.HandleFunc("/api/v1/snapshots", SnapshotServe)
 	http.HandleFunc("/api/v1/forward", ForwardServe)
