c5e6ca3c35deafe14c810fa93d80bb9ac4681fe9
winlin <winlin@vip.126.com>
2023-01-15 13:54:24 +0800

    Refine on_forward API.

--- trunk/research/api-server/server.go
+++ trunk/research/api-server/server.go
@@ -560,65 +560,27 @@ an int value specifies the error code(0 corresponding to success):
 	  0
 */
 
-type SrsForwardMsg struct {
+type SrsForwardRequest struct {
 	SrsCommonRequest
-	TcUrl    string `json:"tc_url"`
-	Stream   string `json:"stream"`
-	Param    string `json:"param"`
+	TcUrl  string `json:"tc_url"`
+	Stream string `json:"stream"`
+	Param  string `json:"param"`
 }
 
-func (v *SrsForwardMsg) String() string {
-	return fmt.Sprintf("srs %v: client id=%v, ip=%v, vhost=%v, app=%v, tcUrl=%v, stream=%v, param=%v", v.Action, v.ClientId, v.Ip, v.Vhost, v.App, v.TcUrl, v.Stream, v.Param)
+func (v *SrsForwardRequest) Parse(b []byte) error {
+	if err := json.Unmarshal(b, v); err != nil {
+		return fmt.Errorf("parse message from %v, err %v", string(b), err)
+	}
+	return nil
 }
 
-type Forward struct{}
-
-/*
-backend service config description:
-   support multiple rtmp urls(custom addresses or third-party cdn service),
-   url's host is slave service.
-For example:
-   ["rtmp://127.0.0.1:19350/test/teststream", "rtmp://127.0.0.1:19350/test/teststream?token=xxxx"]
-*/
-func (v *Forward) Parse(body []byte) (se *SrsError) {
-	msg := &SrsForwardMsg{}
-	if err := json.Unmarshal(body, msg); err != nil {
-		return &SrsError{Code: error_system_parse_json, Data: fmt.Sprintf("parse forward msg failed, err is %v", err.Error())}
+func (v *SrsForwardRequest) String() string {
+	var sb strings.Builder
+	sb.WriteString(v.SrsCommonRequest.String())
+	if v.Action == "on_forward" {
+		sb.WriteString(fmt.Sprintf(", tcUrl=%v, stream=%v, param=%v", v.TcUrl, v.Stream, v.Param))
 	}
-	if msg.Action == "on_forward" {
-		log.Println(msg)
-		res := &struct {
-			Urls []string `json:"urls"`
-		}{
-			Urls: []string{"rtmp://127.0.0.1:19350/test/teststream"},
-		}
-		return &SrsError{Code: 0, Data: res}
-	} else {
-		return &SrsError{Code: error_request_invalid_action, Data: fmt.Sprintf("invalid action:%v", msg.Action)}
-	}
-	return
-}
-
-func ForwardServe(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "GET" {
-		res := struct{}{}
-		body, _ := json.Marshal(res)
-		w.Write(body)
-	} else if r.Method == "POST" {
-		body, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			Response(&SrsError{Code: error_system_read_request, Data: fmt.Sprintf("read request body failed, err is %v", err)}).ServeHTTP(w, r)
-			return
-		}
-		log.Println(fmt.Sprintf("post to forward, req=%v", string(body)))
-		c := &Forward{}
-		if se := c.Parse(body); se != nil {
-			Response(se).ServeHTTP(w, r)
-			return
-		}
-		Response(&SrsError{Code: 0, Data: nil}).ServeHTTP(w, r)
-		return
-	}
+	return sb.String()
 }
 
 func main() {
@@ -824,7 +786,37 @@ func main() {
 	})
 
 	http.HandleFunc("/api/v1/snapshots", SnapshotServe)
-	http.HandleFunc("/api/v1/forward", ForwardServe)
+
+	// handle the dynamic forward requests: on_forward stream.
+	http.HandleFunc("/api/v1/forward", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "POST" {
+			SrsWriteDataResponse(w, struct{}{})
+			return
+		}
+
+		if err := func() error {
+			body, err := ioutil.ReadAll(r.Body)
+			if err != nil {
+				return fmt.Errorf("read request body, err %v", err)
+			}
+			log.Println(fmt.Sprintf("post to forward, req=%v", string(body)))
+
+			msg := &SrsForwardRequest{}
+			if err := msg.Parse(body); err != nil {
+				return err
+			}
+			log.Println(fmt.Sprintf("Got %v", msg.String()))
+
+			SrsWriteDataResponse(w, &SrsCommonResponse{Code: 0, Data: &struct {
+				Urls []string `json:"urls"`
+			}{
+				Urls: []string{"rtmp://127.0.0.1:19350/test/teststream"},
+			}})
+			return nil
+		}(); err != nil {
+			SrsWriteErrorResponse(w, err)
+		}
+	})
 
 	addr := fmt.Sprintf(":%v", port)
 	log.Println(fmt.Sprintf("start listen on:%v", addr))
