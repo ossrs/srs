3a85ae78239e6712686062713c3cbb6f354e884f
hondaxiao <hondaxiao@tencent.com>
2023-01-17 15:05:14 +0800

    RTC: fix rtc publisher pli cid

--- trunk/src/app/srs_app_rtc_source.cpp
+++ trunk/src/app/srs_app_rtc_source.cpp
@@ -703,7 +703,7 @@ srs_error_t SrsRtcSource::on_timer(srs_utime_t interval)
 
     for (int i = 0; i < (int)stream_desc_->video_track_descs_.size(); i++) {
         SrsRtcTrackDescription* desc = stream_desc_->video_track_descs_.at(i);
-        publish_stream_->request_keyframe(desc->ssrc_);
+        publish_stream_->request_keyframe(desc->ssrc_, publish_stream_->context_id());
     }
 
     return err;
