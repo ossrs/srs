20887ae1a7c8db667885996108909375cb6fa026
xiaoq_bj <xiaoq_bj@126.com>
2023-03-21 17:04:48 +0800

    solve srt listen bug

--- trunk/src/app/srs_app_config.cpp
+++ trunk/src/app/srs_app_config.cpp
@@ -7748,7 +7748,7 @@ std::string SrsConfig::get_srt_type(SrsConfDirective* srt_conf)
 
 unsigned short SrsConfig::get_srt_port(SrsConfDirective* srt_conf)
 {
-    SRS_OVERWRITE_BY_ENV_INT("srs.srt_server.pushport"); // SRS_SRT_SERVER_LISTEN
+    SRS_OVERWRITE_BY_ENV_INT("srs.srt_server.listen"); // SRS_SRT_SERVER_LISTEN
 
     static unsigned short DEFAULT = 10080;
 
