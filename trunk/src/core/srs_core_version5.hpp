60afb353c203a6ea8ee18ab0439129db5fbc25f8
winlin <winlin@vip.126.com>
2023-03-25 09:40:56 +0800

    Update release to v5.0.149, v6.0.37

--- trunk/src/core/srs_core_version5.hpp
+++ trunk/src/core/srs_core_version5.hpp
@@ -9,6 +9,6 @@
 
 #define VERSION_MAJOR       5
 #define VERSION_MINOR       0
-#define VERSION_REVISION    148
+#define VERSION_REVISION    149
 
 #endif
--- trunk/src/core/srs_core_version6.hpp
+++ trunk/src/core/srs_core_version6.hpp
@@ -9,6 +9,6 @@
 
 #define VERSION_MAJOR       6
 #define VERSION_MINOR       0
-#define VERSION_REVISION    36
+#define VERSION_REVISION    37
 
 #endif
