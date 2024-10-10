15d6458b854ec2f02058b2a82d5e19b22c870420
zx <2200013083@stu.pku.edu.cn>
2024-10-10 19:04:26 +0800

    check for param should not cover the append of stream

--- trunk/src/protocol/srs_protocol_utility.cpp
+++ trunk/src/protocol/srs_protocol_utility.cpp
@@ -55,11 +55,9 @@ void srs_discovery_tc_url(string tcUrl, string& schema, string& host, string& vh
     // Standard URL is:
     //     rtmp://ip/app/app2/stream?k=v
     // Where after last slash is stream.
+    fullUrl += stream.empty() ? "/" : (stream.at(0) == '/' ? stream : "/" + stream);
     if (fullUrl.find_first_of("?#") == string::npos)
-    {
         fullUrl += param.empty() ? "" : (param.at(0) == '?' ? param : "?" + param);
-        fullUrl += stream.empty() ? "/" : (stream.at(0) == '/' ? stream : "/" + stream);
-    }
 
     // First, we covert the FMLE URL to standard URL:
     //      rtmp://ip/app/app2?k=v/stream , or:
