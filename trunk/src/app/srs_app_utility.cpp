821f39c72b9e45e4144dfda58007eedf94c0a57c
qyt <486179@qq.com>
2025-03-17 11:03:26 +0800

    style modify

--- trunk/src/app/srs_app_utility.cpp
+++ trunk/src/app/srs_app_utility.cpp
@@ -58,18 +58,18 @@ SrsLogLevel srs_get_log_level_v2(string level)
     if ("verbose" == level) {
         return SrsLogLevelVerbose;
     } else if ("debug" == level) {
-		return SrsLogLevelInfo;
-	} else if ("info" == level) {
-		return SrsLogLevelInfo;
-	} else if ("trace" == level) {
-		return SrsLogLevelTrace;
-	} else if ("warn" == level) {
-		return SrsLogLevelWarn;
-	} else if ("error" == level) {
-		return SrsLogLevelError;
-	} else {
-		return SrsLogLevelDisabled;
-	}
+        return SrsLogLevelInfo;
+    } else if ("info" == level) {
+        return SrsLogLevelInfo;
+    } else if ("trace" == level) {
+        return SrsLogLevelTrace;
+    } else if ("warn" == level) {
+        return SrsLogLevelWarn;
+    } else if ("error" == level) {
+        return SrsLogLevelError;
+    } else {
+        return SrsLogLevelDisabled;
+    }
 }
 
 string srs_path_build_stream(string template_path, string vhost, string app, string stream)
