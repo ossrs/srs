c3e7e9ddc8f546c4829b8435f24cce13620fd033
Haibo Chen <495810242@qq.com>
2025-04-02 09:08:10 +0800

    remove unused func: SrsRtcFormatSenders

--- trunk/research/players/js/srs.sdk.js
+++ trunk/research/players/js/srs.sdk.js
@@ -686,36 +686,7 @@ function SrsRtcWhipWhepAsync() {
     return self;
 }
 
-// Format the codec of RTCRtpSender, kind(audio/video) is optional filter.
-// https://developer.mozilla.org/en-US/docs/Web/Media/Formats/WebRTC_codecs#getting_the_supported_codecs
-function SrsRtcFormatSenders(senders, kind) {
-    var codecs = [];
-    senders.forEach(function (sender) {
-        var params = sender.getParameters();
-        params && params.codecs && params.codecs.forEach(function(c) {
-            if (kind && sender.track.kind !== kind) {
-                return;
-            }
-
-            if (c.mimeType.indexOf('/red') > 0 || c.mimeType.indexOf('/rtx') > 0 || c.mimeType.indexOf('/fec') > 0) {
-                return;
-            }
-
-            var s = '';
-
-            s += c.mimeType.replace('audio/', '').replace('video/', '');
-            s += ', ' + c.clockRate + 'HZ';
-            if (sender.track.kind === "audio") {
-                s += ', channels: ' + c.channels;
-            }
-            s += ', pt: ' + c.payloadType;
-
-            codecs.push(s);
-        });
-    });
-    return codecs.join(", ");
-}
-
+// https://developer.mozilla.org/en-US/docs/Web/API/RTCStatsReport
 function SrsRtcFormatStats(stats, kind) {
     var codecs = [];
     stats.forEach((report) => {
