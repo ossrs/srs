3942f5b1cd65c17fc1e238b3e54efc4c7368c431
Artem Smorodin <artem.smorodin@dacast.com>
2025-11-21 16:20:41 +0500

    ignore fmle start packet after flash publish

--- trunk/src/app/srs_app_rtmp_conn.cpp
+++ trunk/src/app/srs_app_rtmp_conn.cpp
@@ -1176,6 +1176,11 @@ srs_error_t SrsRtmpConn::handle_publish_message(SrsSharedPtr<SrsLiveSource> &sou
 
         // for flash, any packet is republish.
         if (info_->type_ == SrsRtmpConnFlashPublish) {
+            if (dynamic_cast<SrsFMLEStartPacket*>(pkt.get())) {
+                srs_warn("flash late FMLE start packet");
+                return err;
+            }
+
             // flash unpublish.
             // TODO: maybe need to support republish.
             srs_trace("flash flash publish finished.");
