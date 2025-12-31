ossrs/srs

commit 550760f2d09c7f0dee2eb10adb94cf9b615ea978

HLS/DASH: Fix dispose to skip unpublish when not enabled, and add forbidden directory protection to SrsPath::unlink. v7.0.111

trunk/src/app/srs_app_dash.cpp
@@ -878,9 +878,13 @@ SrsDash::~SrsDash()
 
 void SrsDash::dispose()
 {
-    if (enabled_) {
-        on_unpublish();
+    // We disabled the reload, so DASH will not be enabled by reloading. 
+    // As a result, if DASH is disabled, we don't need to dispose.
+    if (!enabled_) {
+        return;
     }
+
+    on_unpublish();
 
     // Ignore when dash_dispose disabled.
     srs_utime_t dash_dispose = config_->get_dash_dispose(req_->vhost_);

trunk/src/app/srs_app_hls.cpp
@@ -2549,9 +2549,13 @@ srs_error_t SrsHls::do_reload(int *reloading, int *reloaded, int *refreshed)
 
 void SrsHls::dispose()
 {
-    if (enabled_) {
-        on_unpublish();
+    // We disabled the reload, so HLS will not be enabled by reloading. 
+    // As a result, if HLS is disabled, we don't need to dispose.
+    if (!enabled_) {
+        return;
     }
+
+    on_unpublish();
 
     // Ignore when hls_dispose disabled.
     // @see https://github.com/ossrs/srs/issues/865

trunk/src/core/srs_core_version7.hpp
@@ -9,6 +9,6 @@
 
 #define VERSION_MAJOR 7
 #define VERSION_MINOR 0
-#define VERSION_REVISION 110
+#define VERSION_REVISION 111
 
 #endif

trunk/src/kernel/srs_kernel_utility.cpp
@@ -539,6 +539,40 @@ bool SrsPath::exists(std::string path)
 
 srs_error_t SrsPath::unlink(std::string path)
 {
+    // Define the directories that are forbidden to delete.
+    std::string forbidden[] = {
+        "./",
+        "../",
+        "/",
+        "/bin",
+        "/boot",
+        "/dev",
+        "/etc",
+        "/home",
+        "/lib",
+        "/lib32",
+        "/lib64",
+        "/libx32",
+        "/lost+found",
+        "/media",
+        "/mnt",
+        "/opt",
+        "/proc",
+        "/root",
+        "/run",
+        "/sbin",
+        "/srv",
+        "/sys",
+        "/tmp",
+        "/usr",
+        "/var",
+    };
+    for (int i = 0; i < sizeof(forbidden) / sizeof(forbidden[0]); i++) {
+        if (path == forbidden[i]) {
+            return srs_error_new(ERROR_SYSTEM_FILE_UNLINK, "unlink forbidden %s", path.c_str());
+        }
+    }
+
     if (::unlink(path.c_str()) < 0) {
         return srs_error_new(ERROR_SYSTEM_FILE_UNLINK, "unlink %s", path.c_str());
     }

commit 9503f5f712ee2714b646449759d1cc358f472c56

HLS/DASH: Fix dispose() to cleanup files after unpublish
When a stream is unpublished, on_unpublish() sets enabled_ to false.
Later when dispose() is called by cycle() after hls_dispose timeout,
the early return on !enabled_ prevents controller_->dispose() from
being called, leaving HLS/DASH files on disk.

This fix ensures controller_->dispose() always runs when hls_dispose
is configured, while still preventing duplicate on_unpublish() calls.

Fixes the regression introduced in commit 550760f2d.

trunk/src/app/srs_app_dash.cpp
@@ -886,13 +886,10 @@ SrsDash::~SrsDash()
 
 void SrsDash::dispose()
 {
-    // We disabled the reload, so DASH will not be enabled by reloading.
-    // As a result, if DASH is disabled, we don't need to dispose.
-    if (!enabled_) {
-        return;
+    // Call on_unpublish only if still enabled to avoid duplicate calls.
+    if (enabled_) {
+        on_unpublish();
     }
-
-    on_unpublish();
 
     // Ignore when dash_dispose disabled.
     srs_utime_t dash_dispose = config_->get_dash_dispose(req_->vhost_);
@@ -900,6 +897,7 @@ void SrsDash::dispose()
         return;
     }
 
+    // Always dispose files when dash_dispose timeout occurs, even if already unpublished.
     controller_->dispose();
 }
 

trunk/src/app/srs_app_hls.cpp
@@ -2717,13 +2717,10 @@ srs_error_t SrsHls::do_reload(int *reloading, int *reloaded, int *refreshed)
 // LCOV_EXCL_START
 void SrsHls::dispose()
 {
-    // We disabled the reload, so HLS will not be enabled by reloading.
-    // As a result, if HLS is disabled, we don't need to dispose.
-    if (!enabled_) {
-        return;
+    // Call on_unpublish only if still enabled to avoid duplicate calls.
+    if (enabled_) {
+        on_unpublish();
     }
-
-    on_unpublish();
 
     // Ignore when hls_dispose disabled.
     // @see https://github.com/ossrs/srs/issues/865
@@ -2732,6 +2729,7 @@ void SrsHls::dispose()
         return;
     }
 
+    // Always dispose files when hls_dispose timeout occurs, even if already unpublished.
     controller_->dispose();
 }
 // LCOV_EXCL_STOP