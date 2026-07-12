7709a3397dafefa43dcd893a641e1622561716df
MagicBear <mb@bilibili.com>
2026-07-12 15:18:37 +0200

    chore: use sctplab/usrsctp as git submodule
    Remove vendored usrsctp tree from the main repo and track
    trunk/3rdparty/usrsctp via submodule (https://github.com/sctplab/usrsctp.git).
    
    Clone with: git submodule update --init trunk/3rdparty/usrsctp

--- trunk/3rdparty/usrsctp/fuzzer/fuzzer_listen.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-export ASAN_OPTIONS=abort_on_error=1:disable_core=0:unmap_shadow_on_exit=1:disable_coredump=0
-ulimit -c unlimited
-mkdir -p CORPUS_LISTEN
-./fuzzer_listen -jobs=36 -timeout=10 -max_len=4086 CORPUS_LISTEN
